f46eb7cb42a3e84a2913d8b82d03554f489349d0
whddnjs1715 <whddnjs1715@gmail.com>
2021-10-15 14:19:08 +0900

    react-router-dom 설치 #7

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import "./App.css";
 import Navb from "./components/head/Nav";
 import Jumbotro from "./components/body/Jumbotro";
 import ShoesListt from "./components/body/ShoesListt";
+import Data from "./utils/data";
 
 function App() {
+  const [shoes, setShoes] = useState(Data);
   return (
     <div className="App">
       <Navb />
       <Jumbotro />
-      <ShoesListt />
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+
+const Card = (props) => {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        alt={"shoes" + props.i}
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+};
 
 export default App;
